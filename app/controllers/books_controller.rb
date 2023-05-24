83faeee713d72e50cf4bec69f44cb70d8856cc73
hayate0116 <83390204+hayate0116@users.noreply.github.com>
2023-05-24 14:30:14 +0000

    [edit]4-25

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,31 @@
+class BooksController < ApplicationController
+  def index
+    @books = Book.all
+    @book = Book.new
+  end
+  
+  def edit
+    
+  end
+  
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    @book.save
+    redirect_to books_path
+  end
+  
+  def show
+    
+  end
+  
+  def destroy
+    
+  end
+  
+  private
+  
+  def book_params
+    params.require(:list).permit(:title, :body, :image)  
+  end
+end
